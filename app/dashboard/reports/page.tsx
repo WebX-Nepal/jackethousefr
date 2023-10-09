601e9e87191d23d0c1ccc58a8feda130af17cbb9
prabash19 <prabash19@gmail.com>
2023-10-09 15:25:33 +0545

    tables added

--- app/dashboard/reports/page.tsx
+++ app/dashboard/reports/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 function Reports() {
-  return <div>Reports</div>;
+  const [data, setData] = useState("1");
+  useEffect(() => {
+    console.log("ASdasd");
+  }, []);
+  return (
+    <div>
+      reports {data} <button onClick={() => setData("2")}>asd</button>
+    </div>
+  );
 }
 
 export default Reports;
--- app/dashboard/sales/page.tsx
+++ app/dashboard/sales/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux/";
+import TableTwo from "../../../components/Table";
 function Sales() {
-  return <>Sales</>;
+  return (
+    <>
+      <TableTwo />
+    </>
+  );
 }
 
 export default Sales;
