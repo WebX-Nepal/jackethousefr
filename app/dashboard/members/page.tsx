147aea08187bd3abdcf4bc04017e9be0d21f24c8
prabash19 <prabash19@gmail.com>
2023-10-17 18:17:09 +0545

    members table done

--- app/dashboard/members/page.tsx
+++ app/dashboard/members/page.tsx
@@ -1,57 +1,67 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useGetAllMembersQuery } from "../../../redux/api/secureApi";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import DataTable from "react-data-table-component";
+
 function Members() {
   const [productData, setProducts] = useState([]);
-  const { data, isSuccess } = useGetAllMembersQuery({});
+  const { data: memberData, isSuccess } = useGetAllMembersQuery({});
   useEffect(() => {
-    if (data && isSuccess) {
-      setProducts(data.member);
+    if (memberData && isSuccess) {
+      setProducts(memberData.member);
     } else {
     }
-  }, [data]);
+  }, [memberData]);
+  const columns = [
+    {
+      name: "SN",
+      cell: (row: any, index: number) => index + 1,
+      width: "90px",
+    },
 
+    {
+      name: "Name",
+      selector: (row: any) => row.name,
+    },
+    {
+      name: "Phone",
+      selector: (row: any) => row.phone,
+    },
+    {
+      name: "Created At",
+      selector: (row: any) => new Date(row.createdAt).toLocaleDateString(),
+    },
+    {
+      name: "Points",
+      selector: (row: any) => row.points,
+    },
+    {
+      name: "Actions",
+      cell: (row: any) => (
+        <button
+          className="bg-blue-500 px-4 py-2 rounded-lg text-white"
+          onClick={() => {
+            alert(row);
+          }}
+        >
+          View
+        </button>
+      ),
+    },
+  ];
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
       <div className="py-6 px-4 md:px-6 xl:px-7.5 flex justify-between">
         <h4 className="text-xl font-semibold text-black ">Members</h4>
       </div>
-      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5 mb-3 p-5">
-        <div className="col-span-1 hidden items-center sm:flex">
-          <p className="font-medium">SN</p>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Name</p>
-        </div>
-        <div className="col-span-2 hidden items-center sm:flex">
-          <p className="font-medium">Phone Number</p>
-        </div>
-        <div className="col-span-2 flex items-center">
-          <p className="font-medium">Points</p>
-        </div>
-      </div>
-
-      {productData?.map((product: any, key: number) => (
-        <div
-          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5 p-5"
-          key={key}
-        >
-          <div className="col-span-1 hidden items-center sm:flex">
-            <p className="text-sm text-black ">{key + 1}</p>
-          </div>
-          <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black ">{product.name}</p>
-          </div>
-          <div className="col-span-2 hidden items-center sm:flex">
-            <p className="text-sm text-black ">{product.phone}</p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="text-sm text-black ">{product.points}</p>
-          </div>
-        </div>
-      ))}
+      <DataTable
+        columns={columns}
+        data={productData}
+        pagination
+        fixedHeader
+        highlightOnHover
+        responsive
+      />
     </div>
   );
 }
