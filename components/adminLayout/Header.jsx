fd2bb6db525cb583ccf346ce2fe737a1f2ddfba2
prabash19 <prabash19@gmail.com>
2023-11-03 11:40:07 +0545

    header responsive done

--- components/adminLayout/Header.jsx
+++ components/adminLayout/Header.jsx
@@ -20,29 +20,28 @@ function Header() {
   const handleSearch = (e) => {
     dispatch(setSearchQuery(e.target.value));
   };
+  const logoWidth = 130;
+  const logoHeight = 130;
   return (
     <div className="flex p-1 shadow-xl sticky top-0 z-40 bg-primary">
       <div className="flex flex-grow items-center justify-between pr-4 py-4 shadow-2 md:px-6 2xl:px-11">
-        {screenWidth <= 640 ? (
-          <>
-            <div
-              className="hover:cursor-pointer mr-1 ml-2"
-              onClick={handleClickLogo}
-            >
-              <Image src={"/smicon.png"} width={35} height={35} alt="logo" />
-            </div>
-          </>
-        ) : (
-          <>
-            <div
-              className="hover:cursor-pointer mr-6"
-              onClick={handleClickLogo}
-            >
-              <Image src={"/logo.svg"} width={130} height={130} alt="logo" />
-            </div>
-          </>
-        )}
-
+        <div
+          className={
+            screenWidth <= 640
+              ? "hover:cursor-pointer mr-1 ml-2"
+              : "hover:cursor-pointer mr-6"
+          }
+          onClick={handleClickLogo}
+        >
+          <div style={{ width: `${logoWidth}px`, height: `${logoHeight}px` }}>
+            <Image
+              src={screenWidth <= 640 ? "/smicon.png" : "/logo.svg"}
+              layout="fill"
+              objectFit="contain"
+              alt="logo"
+            />
+          </div>
+        </div>
         <div className="ml-4 flex flex-grow px-4 rounded-xl border border-gray-600 py-2 items-center justify-between max-w-5xl">
           <input
             className="outline-none placeholder-gray-500 bg-transparent text-black w-full"
